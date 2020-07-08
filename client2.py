9e948f50d1b7930f76bb44c7fd77dfe5897ba563
Grant Powell <gjamespowell@gmail.com>
2020-07-08 00:55:49 -0400

    Add in shelter in place example

--- client2.py
+++ client2.py
@@ -5,6 +5,7 @@ import socket
 import ssl
 import struct
 import time
+import base64
 
 # LOAD SETTINGS
 LOAD_KEY_ERROR_MSG = """
@@ -72,6 +73,22 @@ class Bot:
         game_id = game_request["game_info"]["game_id"]
         self.connection.send_message({"action": "accept_game", "game_id": game_id})
 
+    def play(self, game_request):
+        game_request = self.process_game_request(game_request)
+
+        while True:
+            msg = self.connection.recieve_message()
+            if msg.get("commands_request"):
+                commands_request = self.process_commands_request(msg["commands_request"])
+                commands = self.commands(commands_request, game_request)
+                self.send_commands(msg["commands_request"]["request_id"], commands)
+            elif msg.get("info"):
+                if msg["info"] == "game_over":
+                    print(msg["result"])
+
+                break
+
+
     def send_commands(self, request_id, commands):
         self.connection.send_message({"action": "send_commands", "request_id": request_id, "commands": commands})
 
@@ -87,28 +104,38 @@ class Bot:
 class RobotGameBot(Bot):
     DEFAULT_ARENA = "robot-game-default"
 
-    def play(self, game_request):
-        while True:
-            msg = self.connection.recieve_message()
-            if msg.get("commands_request"):
-                commands = self.commands(
-                    msg["commands_request"],
-                    game_request["game_info"]["player"],
-                    game_request["game_info"]["settings"]
-                )
-                self.send_commands(msg["commands_request"]["request_id"], commands)
-            elif msg.get("info"):
-                if msg["info"] == "game_over":
-                    print(msg["result"])
+    @classmethod
+    def guard(cls, robot):
+        return {"type": "guard", "robot_id": robot["id"]}
 
-                break
+    @classmethod
+    def explode(cls, robot):
+        return {"type": "explode", "robot_id": robot["id"]}
 
+    @classmethod
+    def move(cls, robot, target):
+        return {"type": "move", "robot_id": robot["id"], "target": target}
+
+    @classmethod
+    def attack(cls, robot, target):
+        return {"type": "attack", "robot_id": robot["id"], "target": target}
+
+    def process_game_request(self, game_request):
+        print(game_request)
+        settings = game_request["game_info"]["settings"]
+        settings["terrain"] = base64.b64decode(settings["terrain_base64"])
+        return settings
+
+    def process_commands_request(self, commands_request):
+        print(commands_request)
+        player = commands_request["player"]
+        robots = commands_request["game_state"]["robots"]
+        my_robots = [robot for robot in robots if robot["player_id"] == player]
+        enemy_robots = [robot for robot in robots if robot["player_id"] != player]
+        return {"player": player, "robots": robots, "my_robots": my_robots, "enemy_robots": enemy_robots}
 
 class ShelterInPlace(RobotGameBot):
     NAME = "shelter-in-place"
 
-    def commands(self, commands_request, player, game_request):
-        print(commands_request)
-        print(player)
-        print(game_request)
-        return []
+    def commands(self, commands_request, settings):
+        return [self.guard(robot) for robot in commands_request["my_robots"]]
