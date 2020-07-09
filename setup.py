b4f4b1af36ba1f4e57d5c4bb4871fc683373d51b
Grant Powell <gjamespowell@gmail.com>
2020-07-08 21:25:22 -0400

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="battle-box-client-py-Grant-James-Powell",
+    version="0.0.1",
+    author="Grant Powell",
+    description="Battle Box Client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FlyingDutchmanGames/battle_box_client_py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
