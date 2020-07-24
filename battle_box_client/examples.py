a16228ccb4288022e1ed3ab4d3bccba632967a9f
Grant Powell <gjamespowell@gmail.com>
2020-07-24 01:56:39 -0400

    Add a kansas example

--- battle_box_client/examples.py
+++ battle_box_client/examples.py
@@ -1,7 +1,30 @@
-from client import RobotGameBot
+from .client import RobotGameBot
 
 class Tortuga(RobotGameBot):
     NAME = "tortuga"
 
     def commands(self, commands_request, settings):
         return [robot.guard() for robot in commands_request["my_robots"]]
+
+class Kansas(RobotGameBot):
+    NAME = "kansas"
+
+    def commands(self, commands_request, settings):
+        row_midpoint = settings["terrain"].rows // 2
+        col_midpoint = settings["terrain"].cols // 2
+
+        moves = []
+
+        for robot in commands_request["my_robots"]:
+            [x, y] = robot.location
+
+            if y > row_midpoint:
+                moves.append(robot.move([x, y - 1]))
+            elif y < row_midpoint:
+                moves.append(robot.move([x, y + 1]))
+            elif x > col_midpoint:
+                moves.append(robot.move([x - 1, y]))
+            else:
+                moves.append(robot.move([x + 1, y]))
+
+        return moves
